84b4d442a4a991f67cc891f552ad1974c2f256c2
velvetops <quantumular@gmail.com>
2019-05-12 01:23:47 +0900

    added a ball

--- main.cpp
+++ main.cpp
@@ -1,17 +1,27 @@
 #include <fstream>
 #include <iostream>
-#include "ray.h"
-#include "vec3.h"
+#include "ray.hpp"
+#include "vec3.hpp"
+
+bool hit_sphere(const vec3& center, float radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0 * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+}
 
 vec3 color(const ray& r) {
+    if (hit_sphere(vec3(0, 0, -1), 0.5, r)) return vec3(1, 0, 0);
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
 int main() {
-    int nx = 512;
-    int ny = 512;
+    int nx = 600;
+    int ny = 300;
     std::ofstream ofs("image.ppm");
     ofs << "P3\n" << nx << " " << ny << "\n255\n";
 
